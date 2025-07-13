6a30d7c60bb505a1eb026380b50c20c2bd0dd086
Taso01-ops <hc4817@wayne.edu>
2025-07-12 23:25:38 -0400

    Update server.js
    So that the connection to DB works locally!!

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-ECHO is on.
+require('dotenv').config(); // Load environment variables from .env file
+
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({ //Database connection
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Failed!!', err.stack); //ERROR
+    return;
+  }
+  console.log('Connected'); //APPROVED
+});
+
+module.exports = db; //export for use in API
+
