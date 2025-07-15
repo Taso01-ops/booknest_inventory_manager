fd3246acdbd31b9d11974ee4f8326ac06e8eab79
Taso01-ops <hc4817@wayne.edu>
2025-07-14 23:14:42 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BookList from './BookList';
 import SearchBar from './SearchBar';
-import DeleteButton from './DeleteButton';
-import AddBook from './AddBook';
-import UpdateBook from './UpdateBook';
-
-
-const fetchBooks = async () => {
-  try {
-    const res = await axios.get('http://localhost:3001/books');
-    setBooks(res.data);
-  } catch (err) {
-    console.error('Failed to fetch books', err);
-  }
-};
-
-useEffect(() => {
-  fetchBooks();
-}, []);
+import AddBook from './AddBook'; // If you plan to use this
+import axios from 'axios';
 
 const App = () => {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    try {
+      const res = await axios.get('http://localhost:3001/books');
+      setBooks(res.data);
+    } catch (err) {
+      console.error('Failed to fetch books', err);
+    }
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
   return (
     <div>
       <h1>Book Inventory</h1>
       <SearchBar setBooks={setBooks} />
       <BookList books={books} setBooks={setBooks} />
+      <AddBook onSuccess={fetchBooks} />
     </div>
   );
 };
-
-<SearchBar setBooks={setBooks} />
-<BookList books={books} setBooks={setBooks} />
-<AddBook onSuccess={fetchBooks} />
 
 export default App;
