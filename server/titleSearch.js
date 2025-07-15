a770ba339f83ba9bb068fd7f90d9f9edeb7cd44e
Taso01-ops <hc4817@wayne.edu>
2025-07-14 23:46:55 -0400

    Update titleSearch.js

--- server/titleSearch.js
+++ server/titleSearch.js
@@ -1,6 +1,7 @@
-app.get('/books/search/:title', (req, res) => {
-  const title = req.params.title;
-  db.query("SELECT * FROM books WHERE title LIKE ?", [`%${title}%`], (err, results) => {
+// Use this instead of /search/:title
+app.get('/books/search', (req, res) => {
+  const title = req.query.title;
+  db.query("SELECT * FROM books WHERE LOWER(title) LIKE LOWER(?)", [`%${title}%`], (err, results) => {
     if (err) return res.status(500).json(err);
     return res.json(results);
   });
