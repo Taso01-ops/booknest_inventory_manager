fd0f99938d0814c122b014f424e2e4c10192b81a
Taso01-ops <hc4817@wayne.edu>
2025-07-14 20:59:29 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import BookList from '../BookList';
-import SearchBar from '../SearchBar';
+import BookList from './BookList';
+import SearchBar from './SearchBar';
 
 const App = () => {
   const [books, setBooks] = useState([]);
