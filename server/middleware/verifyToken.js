af27fd5b01afdf6518623868ccecac42dd448a92
Taso01-ops <hc4817@wayne.edu>
2025-07-20 15:54:14 -0400

    Update verifyToken.js

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,1 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
+
+  if (!token) return res.status(401).json({ error: 'Access denied. No token provided.' });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ error: 'Invalid token.' });
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = verifyToken;
  
