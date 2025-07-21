90598d03e2cf326ce451bc7da2db91939d4a3a4b
Taso01-ops <hc4817@wayne.edu>
2025-07-20 20:54:42 -0400

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,1 +1,11 @@
- 
+const db = require('../db');
+
+const createUser = (user) => {
+  const sql = 'INSERT INTO customers (name, email, phone, address, password) VALUES (?, ?, ?, ?, ?)';
+  return db.promise().execute(sql, [user.name, user.email, user.phone, user.address, user.password]);
+};
+
+const findUserByEmail = (email) => {
+  const sql = 'SELECT * FROM customers WHERE email = ?';
+  return db.promise().execute(sql, [email]);
+}; 
