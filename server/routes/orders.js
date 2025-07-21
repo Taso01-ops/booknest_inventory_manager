ebea5764179c36082ac0e8683b6ed0f1feab8438
Taso01-ops <hc4817@wayne.edu>
2025-07-21 08:07:26 -0400

    Update orders.js

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { placeOrder, getUserOrders } = require('../controllers/orderController');
+const verifyToken = require('../middleware/verifyToken');
+
+router.post('/', verifyToken, placeOrder);
+router.get('/', verifyToken, getUserOrders);
+
+module.exports = router;
 
