08fb3174f508f5009a57de8f7455a110a3e3fac4
Taso01-ops <hc4817@wayne.edu>
2025-07-21 08:07:15 -0400

    Update orderController.js

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,1 +1,26 @@
+const db = require('../db');
+const { createOrder, getOrdersByCustomerId } = require('../models/orderModel');
+
+exports.placeOrder = (req, res) => {
+  const userId = req.user.userId;
+  const items = req.body.items;
+
+  // Deduct stock
+  items.forEach(item => {
+    db.query('UPDATE books SET stock = stock - ? WHERE id = ?', [item.quantity, item.book_id]);
+  });
+
+  createOrder(userId, items, (err) => {
+    if (err) return res.status(500).json(err);
+    res.status(201).json({ message: 'Order placed' });
+  });
+};
+
+exports.getUserOrders = (req, res) => {
+  const userId = req.user.userId;
+  getOrdersByCustomerId(userId, (err, results) => {
+    if (err) return res.status(500).json(err);
+    res.json(results);
+  });
+};
 
