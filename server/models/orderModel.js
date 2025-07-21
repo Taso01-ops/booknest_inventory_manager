eb0691f645a56ae184b6689f7ca981df485f3d1c
Taso01-ops <hc4817@wayne.edu>
2025-07-21 08:07:02 -0400

    Update orderModel.js

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -1,1 +1,24 @@
+const db = require('../db');
+
+exports.createOrder = (userId, items, callback) => {
+  const date = new Date();
+  db.query('INSERT INTO orders (customer_id, order_date) VALUES (?, ?)', [userId, date], (err, result) => {
+    if (err) return callback(err);
+    const orderId = result.insertId;
+
+    const values = items.map(item => [orderId, item.book_id, item.quantity]);
+    db.query('INSERT INTO order_items (order_id, book_id, quantity) VALUES ?', [values], callback);
+  });
+};
+
+exports.getOrdersByCustomerId = (customerId, callback) => {
+  db.query(`
+    SELECT o.id, o.order_date, b.title, b.price, oi.quantity
+    FROM orders o
+    JOIN order_items oi ON o.id = oi.order_id
+    JOIN books b ON oi.book_id = b.id
+    WHERE o.customer_id = ?
+    ORDER BY o.order_date DESC
+  `, [customerId], callback);
+};
 
