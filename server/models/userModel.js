72b82f1c5c3a3e59d682762060abf1b54cf469a0
Taso01-ops <hc4817@wayne.edu>
2025-07-22 12:18:42 -0400

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,9 +1,11 @@
 const db = require('../db');
 
+// Find a customer by email
 const findUserByEmail = (email, callback) => {
   db.query('SELECT * FROM customers WHERE email = ?', [email], callback);
 };
 
+// Create a new customer
 const createUser = (user, callback) => {
   const { name, email, phone, address, password } = user;
   db.query(
